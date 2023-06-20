1777015826367c3bf15450005e4d4dfa6fbb2135
LordOfLA <LordOfLA@users.noreply.github.com>
2023-06-20 04:13:42 +0100

    feat: Update zsh scripts to also install powerlevel10k and copy zshrc template to home folder.

--- scripts/post/97-oh-my-zsh.sh
+++ scripts/post/97-oh-my-zsh.sh
@@ -8,6 +8,7 @@ set -oue pipefail
 HOME=/tmp RUNZSH=no CHSH=no ZSH=/usr/lib/ohmyzsh \
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" \
 && set -x \
+&& git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-/usr/lib/ohmyzsh/custom}/themes/powerlevel10k \
 && wget -qO /usr/lib/ohmyzsh/custom/kube-ps1.plugin.zsh \
     https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/kube-ps1/kube-ps1.plugin.zsh \
 && git clone https://github.com/zsh-users/zsh-history-substring-search \
