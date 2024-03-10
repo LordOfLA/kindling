67bc716a9961e9e55c6468c8648027a29efda21f
LordOfLA <1672069-lordofla@users.noreply.gitlab.com>
2024-03-10 07:21:27 +0000

    See if this ensures the docker and vscode repos exist for rpm...

--- config/scripts/00-move-files.sh
+++ config/scripts/00-move-files.sh
@@ -6,9 +6,9 @@
 set -oue pipefail
 
 rsync -avh /usr/etc/fan2go/ /etc/fan2go
-rsync -avh /usr/etc/modules-load.d/ /etc/modules-load.d
-rsync -avh /usr/etc/nvidia-container-runtime/ /etc/nvidia-container-runtime
-rsync -avh /usr/etc/sway/ /etc/sway
-rsync -avh /usr/etc/sysconfig/ /etc/sysconfig
-rsync -avh /usr/etc/udev/ /etc/udev
+#rsync -avh /usr/etc/modules-load.d/ /etc/modules-load.d
+#rsync -avh /usr/etc/nvidia-container-runtime/ /etc/nvidia-container-runtime
+#rsync -avh /usr/etc/sway/ /etc/sway
+#rsync -avh /usr/etc/sysconfig/ /etc/sysconfig
+#rsync -avh /usr/etc/udev/ /etc/udev
 rsync -avh /usr/etc/yum.repos.d/ /etc/yum.repos.d
